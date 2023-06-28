a7ad64e1e917f9047b6b71593a8fef7983772bcb
Sushant Singh <sushantbibhu@gmail.com>
2023-06-28 17:45:14 +0530

    add-eventlistner

---
+++ script.js
@@ -0,0 +1,9 @@
+var feet = document.getElementById("feet");
+var inch = document.getElementById("inch");
+
+feet.addEventListener('input', function(){
+    let f = this.value;
+    let i = f*12;
+    inch.value = i;
+});
+
